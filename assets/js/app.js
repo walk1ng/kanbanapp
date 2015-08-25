03811603eb92f7b9aabdff02863518e73bf7a0c1
walk1ng <iliwgg@gmail.com>
2015-08-25 23:49:00 +0800

    add and edit notes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,45 @@ var React = require('./react.js');
 var uuid = require("node-uuid");
 var Notes = require("./notes");
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'learn react',
+				},
+				{
+					id: uuid.v4(),
+					task: 'learn webpack',
+				},
+				{
+					id: uuid.v4(),
+					task: 'washing clothes',
+				}
+			]
+		}
+	},
 	renderNote:function(note){
 			return (
 				<li key={note.id}>{note.task}</li>
 			)
 	},
+	addNote:function(){
+		this.setState({
+			notes: this.state.notes.concat([
+				{
+					id: uuid.v4(),
+					task: "new task",
+				}
+			])
+		})
+	},
 	render:function(){
-		const notes = [
-			{
-				id: uuid.v4(),
-				task: 'learn react',
-			},
-			{
-				id: uuid.v4(),
-				task: 'learn webpack',
-			},
-			{
-				id: uuid.v4(),
-				task: 'washing clothes',
-			}
-		];
 		return (
-			<Notes items={notes}/>
+			<div>
+			<button onClick={this.addNote}>+</button>
+			<Notes items={this.state.notes}/>
+			</div>
 		)
 	}
 })
--- assets/js/note.js
+++ assets/js/note.js
@@ -1,8 +1,57 @@
 var React = require('./react.js');
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			editing:false,
+			taskdetail: this.props.task,
+		}
+	},
+
+	renderTask:function(){
+		return (
+			<div onClick={this.edit}>{this.state.taskdetail}</div>
+		)
+	},
+
+	renderEdit:function(){
+		return (
+			<input type='text' 
+				autoFocus={true} 
+				onBlur={this.finishEdit} 
+				onKeyPress={this.checkEnter}
+				onChange={this.changeHandler}
+				value={this.state.taskdetail} />
+		)
+	},
+
+	edit:function(){
+		this.setState({
+			editing: true,
+		})
+	},
+	changeHandler:function(e){
+		this.setState({
+			taskdetail: e.target.value,
+		})
+	},
+
+	checkEnter:function(e){
+		if(e.key === 'Enter'){
+			this.finishEdit()
+		}
+	},
+
+	finishEdit:function(){
+		this.setState({
+			editing: false,
+		})
+	},
+
 	render:function(){
 		return (
-			<div>{this.props.task}</div>
+			<div>
+				{this.state.editing ? this.renderEdit():this.renderTask()}
+			</div>
 		)
 	}
 })
